3a1e2bed7a1b12ed64d046799f1ae468c7e4fcde
ricar <ricardomontserrat1b@gmail.com>
2024-09-06 02:08:59 -0500

    Yep, I'm lucky it somehow reduce the approximation, since this is a literal circle, not linear regression

--- train.py
+++ train.py
@@ -1,7 +1,4 @@
-from utils import check_path_exists, time_import, timeit, print_computation_times
-
-time_import('pandas')
-import pandas as pd
+from utils import time_import, timeit, print_computation_times, read_and_parse
 
 time_import('numpy')
 import numpy as np
@@ -12,53 +9,33 @@ from tqdm import trange
 FILENAME = 'osu'
 SEED = "minecraft"
 
-
-def parse_params(params: list[str]) -> int | list[int]:
-    if ',' in params[0]:
-        return list(map(int, params[0].split(',')))
-    return int(params[0].strip('\n ') == 'right') + 1
-
-
-@timeit
-def read_and_parse(filename: str) -> pd.DataFrame:
-    check_path_exists(f'{filename}.pkl', f"File '{filename}.pkl' does not exist!")
-
-    dtype = np.float32
-
-    df: pd.DataFrame = pd.read_pickle(f'{filename}.pkl')
-    df = df[['bytes', 'action', 'params']]
-    df.bytes = df.bytes.apply(lambda x: x.astype(dtype))
-    df.action = df.action.map({'Move': 1, 'ButtonUp': 2, 'ButtonDown': 3})
-    df.params = df.params.map(parse_params).where(df.action == 1).ffill()
-    df['output'] = df.apply(lambda row: np.array(row.params + [row.action], dtype), axis=1)
-
-    return df[['bytes', 'output']]
-
-
 # TODO: Maybe normalize the image, by putting pixels together lol
 
-train_data = read_and_parse(FILENAME)
+xs, ys = read_and_parse(FILENAME)
 
-xs = np.stack(train_data.bytes)
-ys = np.stack(train_data.output)
 
 def get_weights(inp: np.ndarray, out: np.ndarray) -> np.ndarray:
     return np.random.random(size=(inp.shape[-1], out.shape[-1]))
+
 
 def forward(x: np.ndarray, w: np.ndarray) -> np.ndarray:
     # print(x.shape, w.shape)
     return x @ w
 
+
 def relu(x: np.ndarray) -> np.ndarray:
     return np.maximum(0, x)
+
 
 def cost(y_hat: np.ndarray, y: np.ndarray) -> np.ndarray:
     err = ((y_hat - y) ** 2).mean(keepdims=True, axis=0)
     # print(err)
     return err
 
+
 def backward(ws: np.ndarray, cst: np.ndarray, lr: float = 0.005) -> np.ndarray:
     return ws - lr * cst
+
 
 @timeit
 def setup(xs: np.ndarray, ys: np.ndarray, normalize: bool = True) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
@@ -78,6 +55,7 @@ def setup(xs: np.ndarray, ys: np.ndarray, normalize: bool = True) -> tuple[np.nd
 
     return xs, ys, ws
 
+
 @timeit
 def train(xs: np.ndarray, ys: np.ndarray, ws: np.ndarray, epochs: int = 100, lr: float = 0.005) -> None:
     for i in trange(epochs):
@@ -90,6 +68,7 @@ def train(xs: np.ndarray, ys: np.ndarray, ws: np.ndarray, epochs: int = 100, lr:
 
     cst = cost(forward(xs, ws), ys)
     print(f'\tFinal Cost: {cst.mean()}')
+
 
 xs, ys, ws = setup(xs, ys, normalize=True)
 train(xs, ys, ws, epochs=1000, lr=0.0005)
--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 import importlib
 import time
-import os
 
 total_import_time = 0
 total_computation_time = 0
+
 
 def time_import(name: str):
     global total_import_time
@@ -15,6 +15,16 @@ def time_import(name: str):
     print(f"""Time elapsed to import {name}: {import_time}""")
     total_import_time += import_time
 
+time_import('numpy')
+import numpy as np
+
+time_import('pandas')
+import pandas as pd
+
+time_import('os')
+import os
+
+
 def timeit(func):
     def wrapper(*args, **kwargs):
         global total_computation_time
@@ -25,18 +35,44 @@ def timeit(func):
         print(f"Time elapsed to run {func.__name__}: {computation_time}")
         total_computation_time += computation_time
         return result
+
     return wrapper
 
+
 @timeit
-def check_path_exists(path: str, err_message: str, fail_on_error = True) -> bool:
+def check_path_exists(path: str, err_message: str, fail_on_error=True) -> bool:
     exists = os.path.exists(path)
     if not exists and fail_on_error:
         print(err_message)
         exit(1)
     return exists
 
+
+def parse_params(params: list[str]) -> int | list[int]:
+    if ',' in params[0]:
+        return list(map(int, params[0].split(',')))
+    return int(params[0].strip('\n ') == 'right') + 1
+
+
+@timeit
+def read_and_parse(filename: str) -> tuple[np.ndarray, np.ndarray]:
+    check_path_exists(f'{filename}.pkl', f"File '{filename}.pkl' does not exist!")
+
+    dtype = np.float32
+
+    df: pd.DataFrame = pd.read_pickle(f'{filename}.pkl')
+    df = df[['bytes', 'action', 'params']]
+    df.bytes = df.bytes.apply(lambda x: x.astype(dtype))
+    df.action = df.action.map({'Move': 1, 'ButtonUp': 2, 'ButtonDown': 3})
+    df.params = df.params.map(parse_params).where(df.action == 1).ffill()
+    df['output'] = df.apply(lambda row: np.array(row.params + [row.action], dtype), axis=1)
+    # df['output'] = df.apply(lambda row: np.array(row.params, dtype), axis=1)
+
+    return np.stack(df.bytes), np.stack(df.output)
+
+
 def print_computation_times():
-    print("-"*100)
+    print("-" * 100)
     total_time = total_import_time + total_computation_time
-    print(f"Import time: {total_import_time} ({total_import_time/total_time:%} of the total time)")
-    print(f"Computation time: {total_computation_time} ({total_computation_time/total_time:%} of the total time)")
+    print(f"Import time: {total_import_time} ({total_import_time / total_time:%} of the total time)")
+    print(f"Computation time: {total_computation_time} ({total_computation_time / total_time:%} of the total time)")
